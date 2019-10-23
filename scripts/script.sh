bc84bef83616815bcd47caf97b58896e27c849d5
Adam Bajger <xbajger@fi.muni.cz>
2019-10-23 16:51:00 +0200

    script lol?

--- scripts/script.sh
+++ scripts/script.sh
@@ -1,5 +1,11 @@
 cd ..
 source venv/bin/activate
-NOW=$(date +"%Y%m%d%H%M%S")
-pv056-split-data -c configs/split/default.json -d datasets.csv > "scripts/log-$NOW-split.log"
+NOW=""
+now(){
+	NOW=$(date +"%Y%m%d%H%M%S")
+}
+now
+# pv056-split-data -c configs/split/default.json -d datasets.csv > "scripts/log-$NOW-split.log"
+now
+pv056-apply-od-methods -c config/od/default.json > "scripts/log-$NOW-od.log"
 deactivate
