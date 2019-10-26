64072cd87085a2de772a2ff7075f09def48d2bdd
Adam Bajger <xbajger@fi.muni.cz>
2019-10-26 13:47:15 +0200

    fixed sequentialization

--- scripts/script.sh
+++ scripts/script.sh
@@ -5,12 +5,13 @@ now(){
 	NOW=$(date +"%Y%m%d-%H%M%S")
 }
 now
-#pv056-split-data -c configs/split/default.json -d datasets.csv > "scripts/log-$NOW-split.log"
+#{ pv056-split-data -c configs/split/default.json -d datasets.csv > "scripts/log-$NOW-split.log"; } &
 wait
 now
-pv056-apply-od-methods -c configs/od/default.json > "scripts/log-$NOW-od.log"
+{ pv056-apply-od-methods -c configs/od/default.json > "scripts/log-$NOW-od.log"; } &
 wait
 now
-pv056-remove-outliers  -c config_remove_outliers_example.json -d datasets.csv
+pv056-remove-outliers  -c config_remove_outliers_example.json -d datasets.csv &
+wait
 
 deactivate
