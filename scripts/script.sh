a415b111370485b791440aac36707f25f68822f6
Adam Bajger <xbajger@fi.muni.cz>
2019-10-26 13:17:34 +0200

    merged od/default.json

--- scripts/script.sh
+++ scripts/script.sh
@@ -5,16 +5,12 @@ now(){
 	NOW=$(date +"%Y%m%d-%H%M%S")
 }
 now
-<<<<<<< HEAD
-pv056-split-data -c configs/split/default.json -d datasets.csv > "scripts/log-$NOW-split.log"
+#pv056-split-data -c configs/split/default.json -d datasets.csv > "scripts/log-$NOW-split.log"
 wait
 now
 pv056-apply-od-methods -c configs/od/default.json > "scripts/log-$NOW-od.log"
-=======
-# pv056-split-data -c configs/split/default.json -d datasets.csv > "scripts/log-$NOW-split.log"
 wait
 now
-pv056-apply-od-methods -c config/od/default.json > "scripts/log-$NOW-od.log"
->>>>>>> bc1946861b0d2e6447fd02fb2d542554796caa80
-wait
+pv056-remove-outliers  -c config_remove_outliers_example.json -d datasets.csv
+
 deactivate
