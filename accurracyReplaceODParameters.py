d5324a959247b1f3e17ffc9708cc7f75713e626d
Adam Bajger <xbajger@fi.muni.cz>
2019-11-04 19:43:01 +0100

    changed the script to be runnable from command line

--- accurracyReplaceODParameters.py
+++ accurracyReplaceODParameters.py
@@ -1,37 +1,64 @@
+import argparse
 import json
 
 
-OD_CONFIG_COLUMN_NAME = "Configuration"
-CLASSIFIER_COLUMN_NAME = "Classifier"
-OD_JSONS_DIRECTORY = "OD_jsons"
-CSV_NAME_INPUT = "accurracyTemp.csv"
-CSV_NAME_OUTPUT = "accuracyTransformed.csv"
+def main():
+    parser = argparse.ArgumentParser(
+        description="Script replaces configuration hashes in an accurracy CSV file \
+                    with actual OD configuration information."
+    )
+    parser.add_argument(
+        "--input-csv",
+        "-i",
+        required=True,
+        help="Filename of CSV file with CLFs accurracies.",
+    )
+    parser.add_argument(
+        "--output-csv",
+        "-o",
+        required=False,
+        default="accurracyTransformed.csv",
+        help="Filename of new, transformed CSV file with CLFs accurracies and OD configurations. Default name is "
+             "accurracyTransformed.csv",
+    )
+    parser.add_argument(
+        "--od-jsons-dir",
+        "-od",
+        required=True,
+        help="Name of a directory containing JSON files for CLF outputs (usually same directory as CLF outputs are in)",
+    )
+    args = vars(parser.parse_args())
+    colname_od_config = "Configuration"
+    colname_classifier = "Classifier"
+    filename_od_json_directory = args["od-jsons-dir"]
+    filename_csv_input = args["input-csv"]
+    filename_csv_output = args["output-csv"]
 
-JSONFiles = dict()
-with open(CSV_NAME_INPUT) as accFile, open(CSV_NAME_OUTPUT, "w") as accFileNew:
-    line = accFile.readline()
-    columnsArr = line.split(",")
-    ODConfigurationColumnIndex = columnsArr.index(OD_CONFIG_COLUMN_NAME)
-    for line in accFile:
-        dataArr = line.split(",")
-        if not len(dataArr) > 1:  # here we check for the final newline in the csv to skip it
-            continue
-        JSON_OD_hash = dataArr[ODConfigurationColumnIndex]
-        classifier = dataArr[columnsArr.index(CLASSIFIER_COLUMN_NAME)].split(".")[-1]
+    dict_json_files = dict()
+    with open(filename_csv_input) as accFile, open(filename_csv_output, "w") as accFileNew:
+        line = accFile.readline()
+        arr_column = line.split(",")
+        index_column_od_configuration = arr_column.index(colname_od_config)
+        for line in accFile:
+            arr_data = line.split(",")
+            if not len(arr_data) > 1:  # here we check for the final newline in the csv to skip it
+                continue
+            json_od_hash = arr_data[index_column_od_configuration]
+            classifier = arr_data[arr_column.index(colname_classifier)].split(".")[-1]
 
-        if JSON_OD_hash not in JSONFiles:
-            JSONFiles[JSON_OD_hash] = open(OD_JSONS_DIRECTORY + ("" if OD_JSONS_DIRECTORY[-1] == "/" else "/") +
-                                           classifier + "_" + JSON_OD_hash + ".json", "r", encoding="UTF-8")
-        else:
-            JSONFiles[JSON_OD_hash].seek(0)
+            if json_od_hash not in dict_json_files:
+                dict_json_files[json_od_hash] = open(
+                    filename_od_json_directory + ("" if filename_od_json_directory[-1] == "/" else "/") +
+                    classifier + "_" + json_od_hash + ".json", "r", encoding="UTF-8")
+            else:
+                dict_json_files[json_od_hash].seek(0)
 
-        #print(line)
+            # print(line)
 
-        od_config = json.load(JSONFiles[JSON_OD_hash])  # loads json configuration as a dict
-        parametersStr = str.join(";", ['"{}":"{}"'.format(k, v) for k, v in od_config["ad_config"]["parameters"].items()])
-        dataArr[ODConfigurationColumnIndex] = parametersStr
+            od_config = json.load(dict_json_files[json_od_hash])  # loads json configuration as a dict
+            od_configuration_human_readable = str.join(";", ['"{}":"{}"'.format(k, v) for k, v in
+                                                             od_config["ad_config"]["parameters"].items()])
+            arr_data[index_column_od_configuration] = od_configuration_human_readable
 
-
-        #print(str.join(",", dataArr))
-        accFileNew.write(str.join(",", dataArr))
-
+            # print(str.join(",", dataArr))
+            accFileNew.write(str.join(",", arr_data))
