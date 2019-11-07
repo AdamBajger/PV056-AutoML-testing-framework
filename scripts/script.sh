6a8e6f78b2fc19939ee873df83125e27ddb7a615
Adam Bajger <xbajger@aura.fi.muni.cz>
2019-11-07 20:42:43 +0100

    config files and shell script

--- scripts/script.sh
+++ scripts/script.sh
@@ -4,21 +4,29 @@ NOW=""
 now(){
 	NOW=$(date +"%Y%m%d-%H%M%S")
 }
+#now
+#TIMESTAMP_SPLIT="$NOW"
+#{ pv056-split-data -c configs/split/default.json -d datasets-split.csv > "scripts/log-$TIMESTAMP_SPLIT-split.log"; } &
+#wait
+#echo "SPLIT completed $NOW"
+#now
+#TIMESTAMP_OD="$NOW"
+#{ pv056-apply-od-methods -c configs/od/default.json > "scripts/log-$TIMESTAMP_OD-od.log"; } &
+#wait
+#echo "OD completed $NOW"
 now
-#{ pv056-split-data -c configs/split/default.json -d datasets.csv > "scripts/log-$NOW-split.log"; } &
-wait
-echo "SPLIT completed $NOW"
-now
-#{ pv056-apply-od-methods -c configs/od/default.json > "scripts/log-$NOW-od.log"; } &
-wait
-echo "OD completed $NOW"
-now
-#{ pv056-remove-outliers  -c configs/rm_o/default.json -d datasets.csv > "scripts/log-$NOW-rm_o.log"; } &
-wait
+TIMESTAMP_RM_O="$NOW"
+pv056-remove-outliers -c configs/rm_o/default.json -d datasets-rm-o.csv > "scripts/log-$TIMESTAMP_RM_O-rm_o.log"
+#wait
 echo "RM O completed $NOW"
 now
+pv056-run-clf -c configs/clf/default.json -d datasets-rm-o.csv > "scripts/log-$NOW-clf.log"
+#wait
+echo "CLF completed $NOW"
+now
+pv056-statistics -r clf_outputs/
+echo "STATS completed $NOW"
 
-{ pv056-run-clf -c configs/clf/default.json -d datasets.csv > "scripts/log-$NOW-clf.log"; } &
-wait
-echo "CLF completed"
+
+
 deactivate
