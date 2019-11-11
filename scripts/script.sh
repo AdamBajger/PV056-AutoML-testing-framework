6e654af26c1f05f6eaf9a2eb6c171bc744dbe39e
Adam Bajger <xbajger@fi.muni.cz>
2019-11-11 17:11:43 +0100

    modified script - csv names are now variables

--- scripts/script.sh
+++ scripts/script.sh
@@ -1,30 +1,34 @@
 cd ..
 source venv/bin/activate
+FILE_DATASET_SPLIT="datasets-split-exp.csv"
+FILE_DATASET_RM_O="datasets-rm-o-exp.csv"
 NOW=""
 now(){
 	NOW=$(date +"%Y%m%d-%H%M%S")
 }
-#now
-#TIMESTAMP_SPLIT="$NOW"
-#{ pv056-split-data -c configs/split/default.json -d datasets-split.csv > "scripts/log-$TIMESTAMP_SPLIT-split.log"; } &
-#wait
-#echo "SPLIT completed $NOW"
-#now
-#TIMESTAMP_OD="$NOW"
-#{ pv056-apply-od-methods -c configs/od/default.json > "scripts/log-$TIMESTAMP_OD-od.log"; } &
-#wait
-#echo "OD completed $NOW"
+now
+TIMESTAMP_SPLIT="$NOW"
+pv056-split-data -c configs/split/default.json -d "$FILE_DATASET_SPLIT" > "scripts/log-$TIMESTAMP_SPLIT-split.log"
+
+echo "SPLIT completed $NOW"
+now
+TIMESTAMP_OD="$NOW"
+pv056-apply-od-methods -c configs/od/default.json > "scripts/log-$TIMESTAMP_OD-od.log"
+echo "OD completed $NOW"
+
 now
 TIMESTAMP_RM_O="$NOW"
-pv056-remove-outliers -c configs/rm_o/default.json -d datasets-rm-o.csv > "scripts/log-$TIMESTAMP_RM_O-rm_o.log"
-#wait
+pv056-remove-outliers -c configs/rm_o/default.json -d "$FILE_DATASET_RM_O" > "scripts/log-$TIMESTAMP_RM_O-rm_o.log"
 echo "RM O completed $NOW"
+
 now
-pv056-run-clf -c configs/clf/default.json -d datasets-rm-o.csv > "scripts/log-$NOW-clf.log"
-#wait
+TIMESTAMP_CLF="$NOW"
+pv056-run-clf -c configs/clf/default.json -d "$FILE_DATASET_RM_O" > "scripts/log-$TIMESTAMP_CLF-clf.log"
 echo "CLF completed $NOW"
+
 now
-pv056-statistics -r clf_outputs/
+TIMESTAMP_ACC="$NOW"
+pv056-statistics -r clf_outputs/ > "accurracy-$TIMESTAMP_ACC.csv"
 echo "STATS completed $NOW"
 
 
